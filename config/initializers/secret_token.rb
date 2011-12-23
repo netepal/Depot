d068f8e92e1e0bbc45d4236711620062510e45a2
netepal <netepal@gmail.com>
2011-12-23 00:32:21 +0000

    Checking again for any possible deletion

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'cbe5e217886dd24149ebd29ad08ccd3af768b6416acde8e9f509afbd86059120daacc3ba47146203ec0590343da97337a6f26a37f43b025ce71f7f4bbabcfc4f'
