9445be576cc6e7ec07155fed57a5e3f6818f152f
netepal <netepal@gmail.com>
2011-12-26 23:37:50 +0000

    Pushing Cart functionality and solved Cart Line items display

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,22 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
-  
-  private
-  
-  def current_cart
-    Cart.find(session[:card_id])
-  rescue ActiveRecord::RecordNotFound
-    cart = Cart.create
-    session[:card_id] = cart_id
-    cart
-  end
+
+
+
+protect_from_forgery
+
+private
+
+def current_cart
+
+Cart.find(session[:cart_id])
+
+rescue ActiveRecord::RecordNotFound
+
+cart = Cart.create
+
+session[:cart_id] = cart.id
+
+cart
+
+end
 end
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,5 +1,7 @@
 class LineItem < ActiveRecord::Base
+has_many :line_items, :dependent => :destroy  
   
+ 
   belongs_to :product
   belongs_to :cart
   
