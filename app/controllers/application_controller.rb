8e722d34c2369c508c200ed4145736fd664c1935
netepal <netepal@gmail.com>
2011-12-26 01:36:55 +0000

    Added Cart functionality and backend processing 122 (26-12-11)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  private
+  
+  def current_cart
+    Cart.find(session[:card_id])
+  rescue ActiveRecord::RecordNotFound
+    cart = Cart.create
+    session[:card_id] = cart_id
+    cart
+  end
 end
